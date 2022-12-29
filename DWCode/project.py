8fb10a3f109dc3e0bc72c2681ef5c41d594fee12
SaiKumarMurakada <121154973+SaiKumarMurakada@users.noreply.github.com>
2022-12-29 11:05:27 +0530

    Update project.py

--- DWCode/project.py
+++ DWCode/project.py
@@ -1,6 +1,6 @@
 #creating folder structure
 import os
-path= 'C:\\Users\\SMURAKAD\\Desktop\\New folder\\new\\DWCode\\CareTeam\\Functions'
+path= 'C:\\Users\\SMURAKAD\\Desktop\\New folder\\new\\DWCode\\CareTeam'
 
   if not os.path.exists(path):
     os.makedirs(path)
